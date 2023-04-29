b7a7070bcad5cc906b4ccbb86f24bd8f866243a8
DavLin <davlin@creatorise.com>
2023-04-29 15:11:51 +0200

    Add require of login form component

---
+++ pages/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../components/login-form/login-form.js"></script>
+    <title>Octopus Booking - Login</title>
+</head>
+
+<body>
+    <h1>Octopus Booking - Login</h1>
+
+    <?php require("../components/login-form/login-form.php") ?>
+</body>
+
+</html>
