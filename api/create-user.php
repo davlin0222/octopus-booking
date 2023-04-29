1a808ef6d2a56eaf1d20de3d6dfb73015bf6bcbe
DavLin <davlin@creatorise.com>
2023-04-29 08:20:51 +0200

    Add create user by api

--- api/create-user.php
+++ api/create-user.php
@@ -2,4 +2,18 @@
 
 require("../src/users.php");
 
-createUser("danieldoe@example.com", "password456", "Daniel", "Doe", 2, "555-5678");
+// define the employee role ID
+$EMPLOYEE_ROLE_ID = 1;
+
+// get the request body as JSON and decode it
+$requestBody = json_decode(file_get_contents('php://input'), true);
+
+// extract the user data from the request body
+$email = $requestBody['email'] ?? '';
+$password = $requestBody['password'] ?? '';
+$firstName = $requestBody['firstName'] ?? '';
+$lastName = $requestBody['lastName'] ?? '';
+$phoneNumber = $requestBody['phoneNumber'] ?? '';
+
+// create the user with the extracted data
+createUser($email, $password, $firstName, $lastName, $phoneNumber, $EMPLOYEE_ROLE_ID);
--- src/users.php
+++ src/users.php
@@ -2,9 +2,21 @@
 
 require("database/execute-query.php");
 
-function createUser($email, $password, $first_name, $last_name, $role_id, $phone_number)
+/**
+ * Create a new user
+ *
+ * @param string $email the email address of the user
+ * @param string $password the password of the user
+ * @param string $first_name the first name of the user
+ * @param string $last_name the last name of the user
+ * @param int $role_id the ID of the user's role
+ * @param string|null $phone_number the phone number of the user (optional)
+ * @return void
+ */
+function createUser($email, $password, $first_name, $last_name, $phone_number, $role_id)
 {
     $password_hash = password_hash($password, PASSWORD_DEFAULT);
-    $query = "INSERT INTO users (email, first_name, last_name, password_hash, role_id, phone_number) VALUES (?, ?, ?, ?, ?, ?)";
-    executeQuery($query, "ssssss", [$email, $first_name, $last_name, $password_hash, $role_id, $phone_number]);
+    $query = "INSERT INTO users (email, first_name, last_name, password_hash, phone_number, role_id) VALUES (?, ?, ?, ?, ?, ?)";
+    executeQuery($query, [$email, $first_name, $last_name, $password_hash, $phone_number, $role_id]);
+    // executeQuery($query, "ssssss", [$email, $first_name, $last_name, $password_hash, $phone_number, $role_id]);
 }
