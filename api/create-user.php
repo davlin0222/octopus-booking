e0cb39b077a3bf3a3ab03c40032c8e7ca494d637
DavLin <davlin@creatorise.com>
2023-04-29 08:52:52 +0200

    Add error handling send as response

--- api/create-user.php
+++ api/create-user.php
@@ -5,27 +5,36 @@ require("../src/users.php");
 // define the employee role ID
 $EMPLOYEE_ROLE_ID = 1;
 
+set_error_handler(function ($errorNumber, $errorString, $errorFile, $errorLine) {
+    // error was suppressed with the @-operator
+    if (0 === error_reporting()) {
+        return false;
+    }
+
+    $exception = new ErrorException($errorString, 0, $errorNumber, $errorFile, $errorLine);
+
+    $response = [
+        "success" => false,
+        "errorMessage" => $exception->getMessage()
+    ];
+    echo json_encode($response);
+    exit();
+});
+
 // get the request body as JSON and decode it
 $requestBody = json_decode(file_get_contents('php://input'), true);
 
 // extract the user data from the request body
-$email = $requestBody['email'] ?? '';
-$password = $requestBody['password'] ?? '';
-$firstName = $requestBody['firstName'] ?? '';
-$lastName = $requestBody['lastName'] ?? '';
-$phoneNumber = $requestBody['phoneNumber'] ?? '';
+$email = $requestBody['email'];
+$password = $requestBody['password'];
+$firstName = $requestBody['firstName'];
+$lastName = $requestBody['lastName'];
+$phoneNumber = $requestBody['phoneNumber'];
 
-try {
-    // create the user with the extracted data
-    createUser($email, $password, $firstName, $lastName, $phoneNumber, $EMPLOYEE_ROLE_ID);
-    $response = [
-        "success" => true
-    ];
-    echo json_encode($response);
-} catch (\Throwable $th) {
-    $response = [
-        "success" => false,
-        "errorMessage" => $th
-    ];
-    echo json_encode($response);
-}
+// create the user with the extracted data
+createUser($email, $password, $firstName, $lastName, $phoneNumber, $EMPLOYEE_ROLE_ID);
+
+$response = [
+    "success" => true
+];
+echo json_encode($response);
