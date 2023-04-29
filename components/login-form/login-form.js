9bc6549f5bead5f7434f00ebe848e450c6ae44e8
DavLin <davlin@creatorise.com>
2023-04-29 15:30:37 +0200

    Add login form on submit prevent submit

--- components/login-form/login-form.js
+++ components/login-form/login-form.js
@@ -1,1 +1,8 @@
 console.log('login-form.js')
+
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', loginForm_onSubmit)
+
+function loginForm_onSubmit(e) {
+    e.preventDefault()
+}
--- pages/login.php
+++ pages/login.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="../components/login-form/login-form.js"></script>
+    <script src="../components/login-form/login-form.js" defer></script>
     <title>Octopus Booking - Login</title>
 </head>
 
 <body>
     <h1>Octopus Booking - Login</h1>
 
-    <?php require("../components/login-form/login-form.php") ?>
+    <?php include("../components/login-form/login-form.php") ?>
 </body>
 
 </html>
