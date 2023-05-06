63c38d63aa9a02d99109babedf7423b6346cf4b2
DavLin <davlin@creatorise.com>
2023-05-06 12:12:07 +0200

    Document authorization

--- src/authorization.php
+++ src/authorization.php
@@ -1,15 +1,16 @@
 <?php
 
+/**
+ * The Authorize class provides methods for checking if a user is logged in and if they have admin privileges.
+ */
 class Authorize
 {
-    public static function isAdmin($notAdminCallback)
-    {
-        if (session_status() == PHP_SESSION_NONE) session_start();
-        if (!$_SESSION["isAdmin"]) {
-            return $notAdminCallback();
-            exit();
-        }
-    }
+    /**
+     * Check if the user is logged in.
+     *
+     * @param callable $notLoggedInCallback A callback function to be executed if the user is not logged in.
+     * @return mixed True if the user is logged in, otherwise the result of the callback function.
+     */
     public static function isLoggedIn($notLoggedInCallback)
     {
         if (session_status() == PHP_SESSION_NONE) session_start();
@@ -17,5 +18,21 @@ class Authorize
             return $notLoggedInCallback();
             exit();
         }
+        return true;
+    }
+    /**
+     * Check if the user is an admin.
+     *
+     * @param callable $notAdminCallback A callback function to be executed if the user is not an admin.
+     * @return mixed True if the user is an admin, otherwise the result of the callback function.
+     */
+    public static function isAdmin($notAdminCallback)
+    {
+        if (session_status() == PHP_SESSION_NONE) session_start();
+        if (!$_SESSION["isAdmin"]) {
+            return $notAdminCallback();
+            exit();
+        }
+        return true;
     }
 }
