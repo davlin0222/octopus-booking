da6172828b9fac7a4c1f00581efed576a888dc50
DavLin <davlin@creatorise.com>
2023-05-06 12:22:52 +0200

    Document redirect

--- src/redirect.php
+++ src/redirect.php
@@ -1,15 +1,22 @@
 <?php
 
-// https://stackoverflow.com/questions/768431/how-do-i-make-a-redirect-in-php
-// 301 - permanent (e.g route to resource moved)
-// 302 - temporary (e.g redirect because not authorized)
+/**
+ * Redirects the user to the specified URL with the specified status code.
+ *
+ * @param string $url The URL to redirect to.
+ * @param bool $permanent Whether the redirect should be permanent (301) or temporary (302).
+ * @return void
+ */
 function redirect($url, $permanent = false)
 {
+    // Determine the appropriate HTTP status code based on whether the redirect is permanent or temporary.
     if ($permanent) {
         $statusCode = 301;
     } else {
         $statusCode = 302;
     }
+
+    // Set the appropriate HTTP headers and redirect the user to the specified URL.
     header('Location: ' . $url, true, $statusCode);
     die();
 }
