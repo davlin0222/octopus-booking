00da33e9c6f1d717ddf62f91ec31b475c36cae30
DavLin <davlin@creatorise.com>
2023-05-06 16:39:47 +0200

    Add date selector changes date get variable

--- components/date-selector/date-selector.js
+++ components/date-selector/date-selector.js
@@ -4,14 +4,16 @@ import { renderBookingChart } from '../booking-chart/booking-chart.js'
 const dateSelectorInput = document.querySelector('#date-selector__input')
 
 const formattedDate = formatDate(new Date())
-console.log(`formattedDate:`, formattedDate)
 dateSelectorInput.value = formattedDate
 
 dateSelectorInput.addEventListener('input', dateSelectorInput_onChange)
 async function dateSelectorInput_onChange(e) {
-    console.log(`dateSelectorInput_onChange  e:`, e)
-
     const dateString = e.target.value
+
+    const params = new URLSearchParams(window.location.search) // get the URL query parameters
+    params.set('date', dateString) // set the 'date' parameter to the selected date string
+    const newUrl = `${window.location.pathname}?${params}` // construct the new URL with the updated query string
+    window.history.replaceState(null, null, newUrl) // replace the current URL with the new one
 
     await renderBookingChart(dateString)
 }
