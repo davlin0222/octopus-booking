e806dc6ff268b2a2a9cff2222faaa3c173a891d2
DavLin <davlin@creatorise.com>
2023-05-06 15:23:01 +0200

    Style menu items

--- components/menu/menu.php
+++ components/menu/menu.php
@@ -1,12 +1,12 @@
-<aside class="page__menu">
+<aside class="page__menu menu">
     <h1 class="heading">Octopus Booking</h1>
-    <nav>
-        <ul>
-            <li>
-                <a href="./booking.php">Booking</a>
+    <nav class="menu__navbar">
+        <ul class="menu__nav-list">
+            <li class="menu__nav-list__item">
+                <a class="menu__nav-list__link" href="./booking.php">Booking</a>
             </li>
-            <li>
-                <button type="button" id="logout-button">Logout</button>
+            <li class="menu__nav-list__item">
+                <button class="menu__nav-list__link" type="button" id="logout-button">Logout</button>
             </li>
         </ul>
     </nav>
