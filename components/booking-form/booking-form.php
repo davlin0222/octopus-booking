fb2400b7c50f34513b30365ba6ebde744788ad11
DavLin <davlin@creatorise.com>
2023-05-07 11:31:59 +0200

    Add an invitation list

--- components/booking-form/booking-form.php
+++ components/booking-form/booking-form.php
@@ -1,4 +1,22 @@
 <form id="booking-form" class="booking-form">
-    <input id="book-button" type="submit" value="Book selected times" class="booking-form__submit _book">
-    <input id="cancel-bookings-button" type="submit" value="Cancel selected bookings" class="booking-form__submit _cancel _hidden">
+    <div class="booking-form__invitations">
+        <ul class="invitations__list">
+            <li class="invitations__list-item">
+                <label class="invitations__label">
+                    <input class="invitations__input" type="checkbox" name="recipients[]" value="david@creatorise.com">
+                    <span class="invitations__text">David</span>
+                </label>
+            </li>
+            <li class="invitations__list-item">
+                <label class="invitations__label">
+                    <input class="invitations__input" type="checkbox" name="recipients[]" value="david@creatorise.com">
+                    <span class="invitations__text">The boss</span>
+                </label>
+            </li>
+        </ul>
+    </div>
+    <div class="booking-form__buttons">
+        <input id="book-button" type="submit" value="Book selected times" class="booking-form__submit _book">
+        <input id="cancel-bookings-button" type="submit" value="Cancel selected bookings" class="booking-form__submit _cancel _hidden">
+    </div>
 </form>
