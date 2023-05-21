e92502a9dabcb05945f65cfd9e44404e11a92442
DavLin <davlin@creatorise.com>
2023-05-21 19:38:27 +0200

    Revert "Improve booking form styling"
    This reverts commit 4c760413216cfb35e25deab432c0a6915f4cd949.

--- components/booking-form/booking-form.php
+++ components/booking-form/booking-form.php
@@ -1,7 +1,5 @@
 <form id="booking-form" class="booking-form">
-
     <div class="booking-form__invitations">
-        <h3 class="invitations__header">Invitations</h3>
         <ul class="invitations__list">
             <template id="invitation-item-template">
                 <li class="invitations__list-item">
@@ -14,7 +12,7 @@
         </ul>
     </div>
     <div class="booking-form__buttons">
-        <input id="book-button" type="submit" value="Confirm booking" class="booking-form__submit _book">
-        <input id="cancel-bookings-button" type="submit" value="Cancel times" class="booking-form__submit _cancel _hidden">
+        <input id="book-button" type="submit" value="Book selected times" class="booking-form__submit _book">
+        <input id="cancel-bookings-button" type="submit" value="Cancel selected bookings" class="booking-form__submit _cancel _hidden">
     </div>
 </form>
