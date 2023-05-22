eeebac14e1573c390a4345eccc06ab72ffb427d1
DavLin <davlin@creatorise.com>
2023-05-22 21:12:12 +0200

    Add user list delete button

--- components/user-list/user-list.php
+++ components/user-list/user-list.php
@@ -2,8 +2,14 @@
     <ul class="user-list">
         <template id="user-list__item-template">
             <li class="user-list__item">
-                <div class="user-list__name"></div>
-                <div class="user-list__email"></div>
+
+                <div class="user-list__info">
+                    <div class="user-list__name"></div>
+                    <div class="user-list__email"></div>
+                </div>
+                <div class="user-list__buttons">
+                    <div class="user-list__delete-button">delete</div>
+                </div>
             </li>
         </template>
     </ul>
