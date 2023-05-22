b6fc21d25577ddb3b629111978068febc8fc2e11
DavLin <davlin@creatorise.com>
2023-05-22 20:18:15 +0200

    Add email addresses to invitations list

--- components/booking-form/booking-form.php
+++ components/booking-form/booking-form.php
@@ -7,7 +7,10 @@
                 <li class="invitations__list-item">
                     <label class="invitations__label">
                         <input class="invitations__input" type="checkbox" name="recipients[]" value="">
-                        <span class="invitations__text"></span>
+                        <div class="invitations__text">
+                            <div class="invitations__name"></div>
+                            <div class="invitations__email"></div>
+                        </div>
                     </label>
                 </li>
             </template>
