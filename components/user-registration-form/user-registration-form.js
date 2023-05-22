21d954103a4438a7487211280350aadee221598c
DavLin <davlin@creatorise.com>
2023-05-23 01:23:26 +0200

    Add redirect after user has been registered

--- components/user-registration-form/user-registration-form.js
+++ components/user-registration-form/user-registration-form.js
@@ -17,13 +17,7 @@ form.addEventListener('submit', async (e) => {
 
     const success = await createUser(userData)
 
-    if (success) {
-        // User created successfully
-        console.log('User created successfully!')
-        // Add any further actions or redirections here
-    } else {
-        // Error occurred while creating user
-        console.error('Error creating user.')
-        // Handle the error or show an appropriate message to the user
-    }
+    if (!success) return
+
+    window.location.href = 'user-management.php'
 })
