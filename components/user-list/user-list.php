fcf7b1e3d67f819785adb99c26540101fb15974c
DavLin <davlin@creatorise.com>
2023-05-22 21:05:43 +0200

    Add email to user list

--- components/user-list/user-list.php
+++ components/user-list/user-list.php
@@ -3,6 +3,7 @@
         <template id="user-list__item-template">
             <li class="user-list__item">
                 <div class="user-list__name"></div>
+                <div class="user-list__email"></div>
             </li>
         </template>
     </ul>
