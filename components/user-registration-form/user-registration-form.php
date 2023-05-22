e6606dc6081d930be1a0debae4e6729bce66dc8d
DavLin <davlin@creatorise.com>
2023-05-23 00:50:59 +0200

    Add user registration form

--- components/user-registration-form/user-registration-form.php
+++ components/user-registration-form/user-registration-form.php
@@ -1,3 +1,21 @@
 <form class="user-registration-form">
-
+    <div class="user-registration-form__field">
+        <label class="user-registration-form__label" for="first_name">First name</label>
+        <input class="user-registration-form__input" type="text" id="first_name" name="first_name" required>
+    </div>
+    <div class="user-registration-form__field">
+        <label class="user-registration-form__label" for="last_name">Last name</label>
+        <input class="user-registration-form__input" type="text" id="last_name" name="last_name" required>
+    </div>
+    <div class="user-registration-form__field">
+        <label class="user-registration-form__label" for="email">Email</label>
+        <input class="user-registration-form__input" type="email" id="email" name="email" required>
+    </div>
+    <div class="user-registration-form__field">
+        <label class="user-registration-form__label" for="password">Password</label>
+        <input class="user-registration-form__input" type="password" id="password" name="password" required>
+    </div>
+    <div class="user-registration-form__field">
+        <input class="user-registration-form__submit" type="submit" value="Register user">
+    </div>
 </form>
