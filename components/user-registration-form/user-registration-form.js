69f119100a6afef1e5c0025f94a0f58b558b920f
DavLin <davlin@creatorise.com>
2023-05-23 00:57:37 +0200

    Add get values of form

--- components/user-registration-form/user-registration-form.js
+++ components/user-registration-form/user-registration-form.js
@@ -1,1 +1,28 @@
-console.log('user-registration-form')
+// import { createUser } from '../../assets/js/users.js'
+
+const form = document.querySelector('.user-registration-form')
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const form = e.target
+
+    const userData = {
+        first_name: form.first_name.value,
+        last_name: form.last_name.value,
+        email: form.email.value,
+        password: form.password.value,
+    }
+    console.log(`form.addEventListener  userData:`, userData)
+
+    // const success = await createUser(userData)
+
+    // if (success) {
+    //     // User created successfully
+    //     console.log('User created successfully!')
+    //     // Add any further actions or redirections here
+    // } else {
+    //     // Error occurred while creating user
+    //     console.error('Error creating user.')
+    //     // Handle the error or show an appropriate message to the user
+    // }
+})
