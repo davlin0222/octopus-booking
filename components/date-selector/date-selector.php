a8cd1a3c55e57ab5c39989a908b5734fbb9e143a
DavLin <davlin@creatorise.com>
2023-05-23 10:36:57 +0200

    Add tooltip to date selector

--- components/date-selector/date-selector.php
+++ components/date-selector/date-selector.php
@@ -1,3 +1,3 @@
-<div class="date-selector">
+<div class="date-selector" title="Keyboard shortcut: Change date by arrow keys">
     <input class="date-selector__input" type="date" name="" id="date-selector__input">
 </div>
